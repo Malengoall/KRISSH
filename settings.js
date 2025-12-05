06b37941dfe545a2e712ed30d3d9cbbda4ac33ae
Malengoall <malengoally87@gmail.com>
2025-12-05 18:21:57 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'KRISSH',
+  author: 'MALENGOALL',
+  botName: "KRISSH",
+  botOwner: 'MALENGOALL', // Your name
+  ownerNumber: '255788072576', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.2",
+  updateZipUrl: "https://github.com/Malengoall/Malengoall-v1/tree/main.Zip",
+};
+
+module.exports = settings;
